89b737a0c819393b116742fbb67b44e2dc91b694
sshi10 <steven.shi@stonybrook.edu>
2024-07-29 00:22:31 -0400

    small edits

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://drive.google.com/file/d/1D_gND_sdAZGD4AZ1pWjjNoRLH9L9tXzT/view?usp=sharing"
+            href=""
           >
             <ArrowIcon />
             <p className="ml-2 h-7">resume</p>
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Welcome to my Personal Portfolio!
       </h1>
       <p className="mb-4">
-        {`I’m Steven, a fourth-year student Computer Engineering student at Stony Brook University. With a diverse set of interests spanning across software, engineering, product management, consulting, data science, and ML/AI, I am constantly driven by the pursuit of personal growth and enrichment.`}<br /><br />
+        {`I’m Steven, a fourth-year Computer Engineering student at Stony Brook University. With a diverse set of interests spanning across software, engineering, product management, consulting, data science, and ML/AI, I am constantly driven by the pursuit of personal growth and enrichment.`}<br /><br />
         {`Recently, I have found myself drawn to the healthcare space, as the community it represents displays a sense of mission and genuine compassion towards humanity. I am committed to doing meaningful work with people, for people.`}<br /><br />
         {`I draw inspiration from the energy and dedication of those around me, and am always eager to apply skills learned from the classroom in a practical setting. I am passionate about contributing to projects that create meaningful social impact, translating to my current role in biomedical informatics research.`}
       </p>
